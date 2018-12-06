bb4303309a66890f6bfa7dcc881609961aeb9461
ajbrown99 <36802611+ajbrown99@users.noreply.github.com>
2018-12-06 17:34:09 -0600

    Add files via upload

--- hash.cpp
+++ hash.cpp
@@ -11,6 +11,7 @@
 
 using namespace std;
 
+//constructor for the hashtable.It sets all the ll's to NULL
 hash::hash(){
 
   for(int i = 0; i < SIZE; i++){
@@ -18,14 +19,17 @@ hash::hash(){
   }  
 }
 
+//return the size of the hashtable
 int hash::getSize(){
 
    return SIZE;
 
 }
 
+//this is the hashing function
 void hash::hashing(vector<string> x,int size,int index){
 
+  //this part computs the key
   int sum = 0;
   int primeMulti = 11;
   int exponent = 1;
@@ -37,8 +41,9 @@ void hash::hashing(vector<string> x,int size,int index){
  }
  
   sum =(sum%size+size)% size;
- // cout << sum << endl;
-
+ 
+  //use the key as the index to the hashtable.insert a ll node
+  //into that index and set the data equal to the file number
   linked_list *node = new linked_list; 
   node->next = NULL;
   node->data = index;
@@ -53,8 +58,11 @@ void hash::hashing(vector<string> x,int size,int index){
 
 }
 
+//this is the function that traverses thru the entire hashtables
+//and then identifies the collisions for each combination of files
 vector<string>hash:: traverse(vector<string>files,int size,int min){
 
+   //create the 2-D array to store the collisions
    vector<string> answer;
    int *matchTable[files.size()];
    for(int i = 0; i < files.size();i++){
@@ -70,7 +78,9 @@ vector<string>hash:: traverse(vector<string>files,int size,int min){
    }
  }
 
-   
+   //traverse thru the entire hashtable
+   //increment the appropriate spot in the 2-D array when a collision
+   //is identified 
    linked_list *ptr;
    linked_list *p;
    for(int i = 0; i < size; i++){
@@ -88,14 +98,9 @@ vector<string>hash:: traverse(vector<string>files,int size,int min){
  
   }
 
- /* for(int i = 0; i < files.size();i++){
-     for(int j = 0; j < files.size();j++){
-
-        cout << matchTable[i][j] << " ";     
-  }
-  cout << endl;
-}
-*/
+   //identify the number of collisions between the 2
+   //files that are greater than the number passed in the command
+   //prompt.Put the numbers into a vector
    vector<int>sortNum;
    for(int i = 0; i < files.size();i++){
       for(int j = 0; j < files.size();j++){
@@ -106,11 +111,15 @@ vector<string>hash:: traverse(vector<string>files,int size,int min){
    }
 }
 
+   //sort the numbers in descending order
    sort(sortNum.begin(),sortNum.end());
    reverse(sortNum.begin(),sortNum.end());
    
+
+   //go thru the 2-D array again and find the filenames that correspond
+   //to the collision number and put them in a string vector that 
+   //will be printed
    vector<int>::iterator find = sortNum.begin();
- 
    while(find != sortNum.end()){
 
      for(int i = 0; i < files.size(); i++){
@@ -132,7 +141,5 @@ vector<string>hash:: traverse(vector<string>files,int size,int min){
      }
      find++;
   }
-//sort(answer.begin(),answer.end());
- //reverse(answer.begin(),answer.end());
  return answer;
 }
